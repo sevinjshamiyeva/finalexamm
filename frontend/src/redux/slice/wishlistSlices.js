0a50a81246b68a851e6e887640273058a64740c4
sevinjshamiyeva <bd84hdbqx@code.edu.az>
2024-03-22 18:52:47 +0400

    lskjdhg

---
+++ frontend/src/redux/slice/wishlistSlices.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'wishlist',
+  initialState,
+  reducers: {
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const {  } = counterSlice.actions
+
+export default counterSlice.reducer
